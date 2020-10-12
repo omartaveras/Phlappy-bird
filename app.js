34b86c6064673d2fda10854a76ab8dee42fa74ae
omartaveras <omar.taverasdiaz@studenti.unicam.it>
2020-10-12 14:40:05 +0100

    Phlappy V0.1

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoader', ()=>{
+    const bird = document.querySelector('.bird');
+    const gameDiplay = document.querySelector('.game-container');
+    const ground = docment.querySelector('.ground');
+});
