5f137361089d1f9a2f509f9d7fcaaf1eaa77cb10
omartaveras <omar.taverasdiaz@studenti.unicam.it>
2020-10-13 22:57:08 +0100

    Phlappy V0.1

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoader', ()=>{
     const ground = docment.querySelector('.ground');
 });
 
+//Function start
+
+// Move up and down
+
+// Game Over
 
 //https://www.youtube.com/watch?v=gxHcW84izz0&feature=youtu.be
 //time: 7:00
